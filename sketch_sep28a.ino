8a85bf6bdf84accd2abcd2f48477e5ca86282161
Saran <30770108+saran-ravi@users.noreply.github.com>
2020-01-02 12:29:45 +0530

    Update sketch_sep28a.ino

--- sketch_sep28a.ino
+++ sketch_sep28a.ino
@@ -64,47 +64,8 @@ forward();
 Serial.println(leftwall);
 Serial.println(frontwall);
 Serial.println(rightwall);
- //right_turn();
- //stop();
- //stopm();
- //delay(2000);
-// if(frontwall==true)
-// { analogWrite(enr,BaseSpeed);
-// analogWrite(enl,BaseSpeed );
-// stop();                        
-// Serial.print("rukooooooooooooooooooooooooooooooooooooooooooooo");
-// }
-//rightfollow();
-//leftrot=0;
-//  rightrot=0;
-//  walls();
  
-//  
-// move_cell();
- //left_turn();
- //delay(2000);
 }
-
- //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-// PID();
-
-//  //u_turn();
-//  left_turn();
-//  stop();
-//  delay(2000);((dis_left<4)&&(dis_right<4))
-//  {
-//   if (dis_left<dis_right)
-//       forward();
-//   else if (dis_left<dis_right)
-//      soft_right();
-//   else if(dis_left>dis_right)
-//       soft_left();
-//   }
-//
-// encoder();
-// ultrasonic();
-// //motors();
-// delay(1000);
 
 void leftfollow()
 {
@@ -183,178 +144,6 @@ void move_cell()
    }
 }
 
-//void move_cell()
-//{
-//  Serial.print("hhhhhhhhhhhhhhhhhhhhhh");
-//  ultrasonic();
-//  a=abs(millis());
-//  Serial.print("a    ");
-//  Serial.println(a);
-//  
-//  int m;
-//  if(rightSensor<60)
-//  {
-//    m=2;
-//  }
-//  if(leftSensor<60)
-//  {
-//    m=1;
-//  }
-//  if(!(leftSensor<60 or rightSensor<60))
-//  {
-//    m=3;
-//  }
-//  Serial.println(m);
-//  switch(m)
-//  {
-//    case 1:
-//    {
-//      while((abs(millis())-a)<5000)
-//      {
-//        Serial.println(abs(millis()));
-//        leftfollow();
-//      }
-//      break;
-//    }
-//    case 2:
-//    {
-//      while((abs(millis())-a)<5000)
-//      {
-//        Serial.println(abs(millis()));
-//        rightfollow();
-//      }
-//      break;
-//    }
-//    case 3:
-//    {
-//       attachInterrupt(digitalPinToInterrupt(encl),leftpulse,CHANGE);
-//       attachInterrupt(digitalPinToInterrupt(encr),rightpulse, CHANGE);
-//      while((abs(millis())-a)<2000)
-//      { 
-//        Serial.println(abs(millis()));
-//        analogWrite(enr,BaseSpeed);
-//        analogWrite(enl,BaseSpeed);
-//        forward();
-//        delay(5);
-//      }
-//      detachInterrupt(digitalPinToInterrupt(encl));
-//      detachInterrupt(digitalPinToInterrupt(encr));
-//      break;
-//    }
-//  }
-//    
-//
-//   stopm();
-//   Serial.println("one finished ............................................................-");
-// }
-//void move_cell()
-//{
-//  leftrot=0;
-//  rightrot=0;
-//  stopm();
-//  delay(1000);
-//
-//  while(leftrot<70 and rightrot<70)
-//  {
-//    ultrasonic();
-//    Serial.println(leftrot);
-//    Serial.println(rightrot);
-//    Serial.println("\n");
-//    analogWrite(enl,110);
-//    analogWrite(enr,110);
-//    
-//  
-//        forward();
-//        delay(10); 
-//  
-//        if((leftrot)<(rightrot-5))
-//        {
-//          analogWrite(enl,80);
-//          analogWrite(enr,0);
-//          soft_right();
-//         delay(5);
-//      
-//        }
-//        if((rightrot)<(leftrot-5))
-//        {
-//          analogWrite(enl,0);
-//          analogWrite(enr,80);
-//         soft_left();
-//         delay(5);
-//        }
-//        if(leftSensor<3.5)
-//        {
-//          analogWrite(enl,80);
-//          analogWrite(enr,0);
-//          soft_right();
-//          delay(25);
-//          
-//        }
-//        else if(rightSensor<3.5)
-//        {
-//          analogWrite(enl,0);
-//          analogWrite(enr,80);
-//          soft_left();
-//          delay(25);
-//          
-//        }
-//        
-//        
-//       
-//     }
-//    
-//}
-//void move_cell()
-//{ leftrot=0;
-//  rightrot=0;
-//  ultrasonic();
-//  walls();
-//  if(rightwall)
-//  {
-//    while(leftrot<1.25 && rightrot<1.25)
-//    { walls();
-//      if(rightwall)
-//      {Serial.println("yes");
-//        errorP=rightSensor-34;
-//        totalError=0.5*errorP;
-//      }
-//      else
-//      {Serial.println("no");
-//        totalError=0;
-//      }
-//      analogWrite(enr,BaseSpeed - totalError);
-//      analogWrite(enl,BaseSpeed + totalError);
-//      forward();
-//      ultrasonic();
-//    }
-//  }
-//  
-//  else if(leftwall)
-//  {
-//    while(leftrot<1.25 && rightrot<1.25)
-//    { 
-//      Serial.print("encoder:");
-//      Serial.println(leftrot);
-//      Serial.println(rightrot);
-//      walls();
-//      ultrasonic();
-//      if(leftwall)
-//      {
-//        errorP=34-leftSensor;
-//        totalError=0.2*errorP;
-//      }
-//      else
-//      {
-//        totalError=0;
-//      }
-//      analogWrite(enr,BaseSpeed - totalError);
-//      analogWrite(enl,BaseSpeed + totalError);
-//      forward();
-//    }
-//    Serial.println("stoppppppppppppppppppppp............................................................................................");
-//  }
-//  stop();
-//}
 void forward()
 {
   digitalWrite(ml2,HIGH);
@@ -502,23 +291,7 @@ void ultrasonic()
   Serial.println(frontSensor   );
   Serial.println(rightSensor   );
 }
-//void read_walls()                                                         // reading values from sensors and assigning it to cwall
-//{ cwall.front = 0;
-//  cwall.right = 0;
-//  cwall.left = 0;
-//  if (dis_left>50)
-//    {
-//      cwall.left = 1;
-//    }
-//    if (dis_middle>50)
-//    {
-//      cwall.front = 1;
-//    }
-//    if (dis_right>50)
-//    {
-//      cwall.right = 1;
-//    }
-//}
+
 void walls() {
 
 
@@ -545,43 +318,6 @@ void walls() {
   }
 
 }
-// void PID(void)       
-// {   
-//     if((leftwall)&&(rightwall))//has both walls
-//     {  //ccw direction is positive
-//         //63 is the offset between left and right sensor when mouse in the middle of cell
-//         errorP = rightSensor-leftSensor+33;
-//         errorD=errorP-oldErrorP;
-//         errorI = (2.0 / 3) * errorI + errorP ;
-//     }        
-//     else if(leftwall)//only has left wall
-//     {   errorP=2*(34-leftSensor);
-        
-//         errorD=errorP-oldErrorP;
-        
-//         errorI = (2.0 / 3) * errorI + errorP ;
-//     }
-//     else if(rightwall)//only has right wall
-//     {
-//         errorP = 2*(rightSensor - rightMiddleValue);
-//         errorD=errorP-oldErrorP;
-//         errorI = (2.0 / 3) * errorI + errorP ;
-//     }
-//     else if((!leftwall)&&(!rightwall))//no wall, use encoder or gyro
-//     {   stopm();
-//         delay(5000);
-//         errorP = 0;//(leftEncoder – rightEncoder*1005/1000)*3;
-//         errorD = 0;
-//         errorI=0;
-//     }
-//     totalError = P * errorP + D * errorD +I*errorI;
-//     oldErrorP = errorP;
-//    // Serial.print("errorrr:");
-//    // Serial.println(totalError/2);
-//  analogWrite(enr,BaseSpeed + totalError/2);
-//  analogWrite(enl,BaseSpeed - totalError/2);
-//     forward();
-// }
 
 
  
